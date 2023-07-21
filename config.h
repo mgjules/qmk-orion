c81a00f42680c6578863eea3fcfd555be7b05554
Michaël Giovanni Jules <me@mgjules.dev>
2023-07-21 19:45:58 +0400

    Genesis

---
+++ config.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define USB_SUSPEND_WAKEUP_DELAY 2000
+
+#undef SPLIT_USB_DETECT
+#define USB_VBUS_PIN 19U
+
+#define FORCE_NKRO
+
+#define FORCED_SYNC_THROTTLE_MS 100
+#define SPLIT_MAX_CONNECTION_ERRORS 10
+#define SPLIT_WATCHDOG_ENABLE
+
+#undef NO_DEBUG
+#define NO_DEBUG
+#undef NO_PRINT
+#define NO_PRINT
+#define NO_ACTION_TAPPING
+#define NO_ACTION_ONESHOT
