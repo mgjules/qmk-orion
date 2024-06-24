aeae4812cd3a5f5b797653b086860d1c73ed85b2
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2024-06-24 17:03:46 +0400

    fix keymap

--- keymap.c
+++ keymap.c
@@ -6,25 +6,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_Q,			KC_W,			KC_F,			KC_P,			KC_B,				KC_J,			KC_L,			KC_U,			KC_Y,			KC_QUOT,
 		KC_A,			KC_R,			KC_S,			KC_T,			KC_G,				KC_M,			KC_N,			KC_E,			KC_I,			KC_O,
 		KC_Z,			KC_X,			KC_C,			KC_D,			KC_V,				KC_K,			KC_H,			KC_COMM,		KC_DOT,			KC_SLSH,
-											MO(2),			KC_SPC,				KC_LSFT,		MO(1)
+											MO(2),			KC_SPC,			    KC_LSFT,		MO(1)
     ),
 	[1] = LAYOUT_split_3x5_2(
-		KC_EXLM,		KC_AT,			KC_HASH,		KC_DLR,			KC_PERC,			KC_CIRC,		KC_AMPR,		KC_ASTR,		KC_TRNS,		KC_TRNS, 
-		KC_GRV,			KC_LT,			KC_LCBR,		KC_LPRN,		KC_LBRC,			KC_PIPE,		KC_COLN,		KC_MINS,		KC_QUOT,		KC_PEQL, 
-		KC_TILD,		KC_GT,			KC_RCBR,		KC_RPRN,		KC_RBRC,			KC_BSLS,		KC_SCLN,		KC_UNDS,		KC_DQUO,		KC_PPLS, 
-											MO(3),			KC_NO,				KC_NO,			KC_NO
+		KC_EXLM,		KC_AT,			KC_HASH,		KC_DLR,			KC_PERC,			KC_CIRC,		KC_AMPR,		KC_ASTR,		KC_TRNS,		KC_TRNS,
+		KC_GRV,			KC_LT,			KC_LCBR,		KC_LPRN,		KC_LBRC,			KC_PIPE,		KC_COLN,		KC_MINS,		KC_QUOT,		KC_EQL,
+		KC_TILD,		KC_GT,			KC_RCBR,		KC_RPRN,		KC_RBRC,			KC_BSLS,		KC_SCLN,		KC_UNDS,		KC_DQUO,		LSFT(KC_EQL),
+											MO(3),			KC_TRNS,			KC_TRNS,		KC_TRNS
     ),
 	[2] = LAYOUT_split_3x5_2(
-		KC_ESC,			QK_RBT,			KC_VOLD,		KC_MUTE,		KC_VOLU,			KC_PGUP,		KC_HOME,		KC_UP,			KC_END,			KC_INS, 
-		KC_LCTL,		KC_LALT,		KC_LGUI,		KC_LSFT,		KC_TAB,				KC_PGDN,		KC_LEFT,		KC_DOWN,		KC_RGHT,		KC_DEL, 
-		LGUI(KC_Z),		LGUI(KC_X),		LGUI(KC_C),		LGUI(KC_V),		LGUI(KC_S),		 	QK_REP,			KC_ENT,		 	KC_PSCR,		KC_SCRL,		KC_BSPC, 
-											KC_NO,			KC_NO,				KC_NO,			MO(3)
+		KC_ESC,			QK_RBT,			KC_VOLD,		KC_MUTE,		KC_VOLU,			KC_PGUP,		KC_HOME,		KC_UP,			KC_END,			KC_INS,
+		KC_LCTL,		KC_LALT,		KC_LGUI,		KC_LSFT,		KC_TAB,				KC_PGDN,		KC_LEFT,		KC_DOWN,		KC_RGHT,		KC_DEL,
+		LGUI(KC_Z),		LGUI(KC_X),		LGUI(KC_C),		LGUI(KC_V),		LGUI(KC_S),		 	QK_REP,			KC_ENT,		 	KC_PSCR,		KC_SCRL,		KC_BSPC,
+											KC_TRNS,		KC_TRNS,			KC_TRNS,		MO(3)
     ),
 	[3] = LAYOUT_split_3x5_2(
-		KC_F1,			KC_F2,			KC_F3,			KC_F4,			KC_F5,				KC_PAST,		KC_7,		  	KC_8,			KC_9,			KC_PPLS, 
-		KC_LCTL,		KC_LALT,		KC_LGUI,		KC_LSFT,		KC_COMM,			KC_PSLS,		KC_4,			KC_5,			KC_6,			KC_PMNS, 
-		KC_F6,			KC_F7,			KC_F8,			KC_F9,			KC_F10,				KC_0,			KC_1,			KC_2,			KC_3,			KC_PEQL, 
-											KC_NO,			KC_NO,				KC_NO,			KC_NO
+		KC_F1,			KC_F2,			KC_F3,			KC_F4,			KC_F5,				KC_PAST,		KC_7,		  	KC_8,			KC_9,			LSFT(KC_EQL),
+		KC_LCTL,		KC_LALT,		KC_LGUI,		KC_LSFT,		KC_COMM,			KC_PSLS,		KC_4,			KC_5,			KC_6,			KC_MINS,
+		KC_F6,			KC_F7,			KC_F8,			KC_F9,			KC_F10,				KC_0,			KC_1,			KC_2,			KC_3,			KC_PEQL,
+											KC_TRNS,		KC_TRNS,			KC_TRNS,		KC_TRNS
     )
 };
 
